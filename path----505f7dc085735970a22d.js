a2318c22a98e6ee3c2f3c32b3268f0da8c2efeeb
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-04-26 08:54:18 +0200

    Updates

---
+++ path----505f7dc085735970a22d.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{129:function(t,a){t.exports={data:{site:{siteMetadata:{title:"the Aurora Platform"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----505f7dc085735970a22d.js.map
