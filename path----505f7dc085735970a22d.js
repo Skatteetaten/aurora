0032f34bea4c8538339e2e29befa2360f356ac00
Espen Strømland <espen.stromland@skatteetaten.no>
2018-04-30 15:52:39 +0200

    Updates

--- path----505f7dc085735970a22d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{129:function(t,a){t.exports={data:{site:{siteMetadata:{title:"the Aurora Platform"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----505f7dc085735970a22d.js.map
---
+++ path----98e6476ef3fbfe6a4941.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{130:function(t,e){t.exports={data:{site:{siteMetadata:{title:"the Aurora Platform",menu:[{href:"/",name:"Home"},{href:"/documentation",name:"Documentation"}]}}},layoutContext:{}}}});
+//# sourceMappingURL=path----98e6476ef3fbfe6a4941.js.map
