55313269330f2d23abc86501463157445025e7a8
Jenkins <nobody@skatteetaten.no>
2018-06-15 14:44:45 +0200

    Updates

--- path----98e6476ef3fbfe6a4941.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{130:function(t,e){t.exports={data:{site:{siteMetadata:{title:"the Aurora Platform",menu:[{href:"/",name:"Home"},{href:"/documentation",name:"Documentation"}]}}},layoutContext:{}}}});
-//# sourceMappingURL=path----98e6476ef3fbfe6a4941.js.map
