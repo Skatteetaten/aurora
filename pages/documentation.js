e87565cedca2443d46c4bb626022d57cd3af24f8
Espen Strømland <espen.stromland@gmail.com>
2018-06-15 09:12:54 +0200

    Feature/starter submodule (#7)
    * clean up and added prop types
    
    * gatsby config
    
    * added Jenkinsfile
    
    * creadentials id
    
    * scriptVersion
    
    * yarn..
    
    * early abort if not master
    
    * design update
    
    * fixed header
    
    * replaced content buttons with StartpageNav
    
    * improved header menu
    
    * toc indent
    
    * added starter submodule
    
    * fixed imports
    
    * refactor
    
    * added rimraf
    
    * aurora frontend komponenter
    
    * format
    
    * Added some docs on how to initialize submodule
    
    * fixed prop
    
    * submodule update
    
    * submodule master branch

---
+++ pages/documentation.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Grid from "aurora-frontend-react-komponenter/Grid";
+import NavigationTile from "aurora-frontend-react-komponenter/NavigationTile";
+import { SingleColumnRow } from "../gatsby-starter-skatteetaten/src/components/Columns";
+import Link from "gatsby-link";
+
+const DocumentationPage = ({
+  data: {
+    allMarkdownRemark: { edges }
+  }
+}) => {
+  const contents = edges
+    .filter(
+      ({ node }) =>
+        node.fields && node.fields.slug.search("/documentation/") >= 0
+    )
+    .map(({ node }) => ({
+      to: node.fields.slug,
+      icon: node.frontmatter.icon,
+      title: node.frontmatter.title,
+      description: node.frontmatter.description || ""
+    }));
+
+  return (
+    <Grid>
+      <SingleColumnRow>
+        <div>
+          <h1>Documentation</h1>
+          <br />
+        </div>
+      </SingleColumnRow>
+      <SingleColumnRow>
+        <NavigationTile
+          contents={contents}
+          renderContent={(to, content) => <Link to={to}>{content}</Link>}
+        />
+      </SingleColumnRow>
+    </Grid>
+  );
+};
+
+export default DocumentationPage;
+
+export const pageQuery = graphql`
+  query DocumentationQuery {
+    allMarkdownRemark(sort: { order: ASC, fields: [frontmatter___title] }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            icon
+            description
+          }
+        }
+      }
+    }
+  }
+`;
--- src/components/Columns/index.js
+++
@@ -1,5 +0,0 @@
-import { SingleColumn, DoubleColumn } from './columns'
-
-export { SingleColumn, DoubleColumn }
-
-export default { SingleColumn, DoubleColumn }
